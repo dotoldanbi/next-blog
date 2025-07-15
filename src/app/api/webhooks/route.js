b5f47443fb89d5b612322fda18f95e970c74c77b
jhsong <jh@song>
2025-07-15 16:19:17 +0900

    webhook

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -3,7 +3,7 @@ import { NextRequest } from "next/server";
 
 export async function POST(req) {
   try {
-    const evt = await verifyWebhook(req);
+    const evt = await verifyWebhook(req, CLERK_WEBHOOK_SECRET);
 
     // Do something with payload
     // For this guide, log payload to console
