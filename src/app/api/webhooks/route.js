feba38d42ca7c5a46df17c81a88a6094fedbe82e
jhsong <jh@song>
2025-07-16 17:35:59 +0900

    route.js

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -8,12 +8,12 @@ export async function POST(req) {
 
     // Do something with payload
     // For this guide, log payload to console
-    const { id } = evt.data;
-    const eventType = evt.type;
+    const { id } = evt?.data;
+    const eventType = evt?.type;
     console.log(
       `Received webhook with ID ${id} and event type of ${eventType}`
     );
-    console.log("Webhook payload:", evt.data);
+    console.log("Webhook payload:", evt?.data);
 
     if (eventType === "user.created" || eventType === "user.updated") {
       const { id, first_name, last_name, image_url, email_address, username } =
